f12f656d514432e431c9cb3676ce9955499e89a1
mamun07 <mamunben7@gmail.com>
2022-09-15 13:28:53 +0600

    ticket details

--- src/app/pages/deshboard/ticket-details-page/ticket-details-page.module.ts
+++ src/app/pages/deshboard/ticket-details-page/ticket-details-page.module.ts
@@ -1,19 +1,28 @@
 import { MaterialModule } from 'src/app/material/material.module';
 import { NgModule } from '@angular/core';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 import { CommonModule } from '@angular/common';
 
 import { TicketDetailsPageRoutingModule } from './ticket-details-page-routing.module';
 import { TicketDetailsPageComponent } from './ticket-details-page.component';
-
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzImageModule } from 'ng-zorro-antd/image';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @NgModule({
-  declarations: [
-    TicketDetailsPageComponent
-  ],
+  declarations: [TicketDetailsPageComponent],
   imports: [
     CommonModule,
     TicketDetailsPageRoutingModule,
-    MaterialModule
-  ]
+    MaterialModule,
+    NzCardModule,
+    NzButtonModule,
+    NzGridModule,
+    NzImageModule,
+    NzInputModule,
+    NzIconModule,
+  ],
 })
-export class TicketDetailsPageModule { }
+export class TicketDetailsPageModule {}
