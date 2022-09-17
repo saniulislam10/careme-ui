b5ec1178ded9f5c705463595dc717e9c9e5c5b51
Saniul Islam <saniul.client@gmail.com>
2022-09-17 11:30:51 +0600

    return & canceled

--- src/app/pages/deshboard/canceled-order/canceled-order-routing.module.ts
+++ src/app/pages/deshboard/canceled-order/canceled-order-routing.module.ts
@@ -1,7 +1,13 @@
+import { CanceledOrderComponent } from './canceled-order.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CanceledOrderComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pages/deshboard/my-return/my-return-routing.module.ts
+++ src/app/pages/deshboard/my-return/my-return-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyReturnComponent } from './my-return.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MyReturnComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
