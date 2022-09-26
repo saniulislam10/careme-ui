96df0e428878b5035fac29273ccf1df366a2af3d
mamun07 <mamunben7@gmail.com>
2022-09-26 20:02:26 +0600

    invoice page

--- src/app/admin/pages/order/all-invoice/all-invoice.component.ts
+++ src/app/admin/pages/order/all-invoice/all-invoice.component.ts
@@ -1,4 +1,3 @@
-
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MatDatepickerInputEvent } from '@angular/material/datepicker';
@@ -14,10 +13,10 @@ import { UtilsService } from 'src/app/services/utils.service';
 @Component({
   selector: 'app-all-invoice',
   templateUrl: './all-invoice.component.html',
-  styleUrls: ['./all-invoice.component.scss']
+  styleUrls: ['./all-invoice.component.scss'],
 })
 export class AllInvoiceComponent implements OnInit {
-
+  tabs = ['All Invoice', 'Overdue', 'Unpaid', 'Open', 'Closed', 'Paid'];
 
   invoices: any;
   currentPage = 1;
@@ -26,48 +25,48 @@ export class AllInvoiceComponent implements OnInit {
   today = new Date();
   dataFormDateRange = new FormGroup({
     start: new FormControl(),
-    end: new FormControl()
+    end: new FormControl(),
   });
   filter: any;
   // sort
-  public sortQuery = {createdAt: -1};
+  public sortQuery = { createdAt: -1 };
   public activeSort = null;
 
   //subscription
   private subAcRoute: Subscription;
-  searchInvoices : any[] = [];
+  searchInvoices: any[] = [];
 
   productOrderStatus: Select[] = [
-    {value: OrderStatus.PENDING, viewValue: 'Pending'},
-    {value: OrderStatus.CONFIRM, viewValue: 'Confirm'},
-    {value: OrderStatus.PROCESSING, viewValue: 'Processing'},
-    {value: OrderStatus.SHIPPING, viewValue: 'Shipping'},
-    {value: OrderStatus.DELIVERED, viewValue: 'Delivered'},
-    {value: OrderStatus.CANCEL, viewValue: 'Cancel'},
-    {value: OrderStatus.REFUND, viewValue: 'Refund'}
+    { value: OrderStatus.PENDING, viewValue: 'Pending' },
+    { value: OrderStatus.CONFIRM, viewValue: 'Confirm' },
+    { value: OrderStatus.PROCESSING, viewValue: 'Processing' },
+    { value: OrderStatus.SHIPPING, viewValue: 'Shipping' },
+    { value: OrderStatus.DELIVERED, viewValue: 'Delivered' },
+    { value: OrderStatus.CANCEL, viewValue: 'Cancel' },
+    { value: OrderStatus.REFUND, viewValue: 'Refund' },
   ];
 
   paymentOrderStatus: Select[] = [
-    {value: PaymentStatus.PAID, viewValue: 'Paid'},
-    {value: PaymentStatus.UNPAID, viewValue: 'Unpaid'},
-    {value: PaymentStatus.PENDING, viewValue: 'Pending'},
-    {value: PaymentStatus.PARTIALPAID, viewValue: 'Partial Paid'},
-    {value: PaymentStatus.REFUNDED, viewValue: 'Refunded'},
-    {value: PaymentStatus.OVERDUE, viewValue: 'Overdue'},
-    {value: PaymentStatus.CANCEL, viewValue: 'Cancel'},
-    {value: PaymentStatus.EXPIRED, viewValue: 'Expired'}
+    { value: PaymentStatus.PAID, viewValue: 'Paid' },
+    { value: PaymentStatus.UNPAID, viewValue: 'Unpaid' },
+    { value: PaymentStatus.PENDING, viewValue: 'Pending' },
+    { value: PaymentStatus.PARTIALPAID, viewValue: 'Partial Paid' },
+    { value: PaymentStatus.REFUNDED, viewValue: 'Refunded' },
+    { value: PaymentStatus.OVERDUE, viewValue: 'Overdue' },
+    { value: PaymentStatus.CANCEL, viewValue: 'Cancel' },
+    { value: PaymentStatus.EXPIRED, viewValue: 'Expired' },
   ];
 
   constructor(
     private invoiceService: InvoiceService,
     private utilsService: UtilsService,
-    private router:Router,
-    private activatedRoute: ActivatedRoute,
-  ) { }
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     // GET PAGE FROM QUERY PARAM
-    this.subAcRoute = this.activatedRoute.queryParams.subscribe(qParam => {
+    this.subAcRoute = this.activatedRoute.queryParams.subscribe((qParam) => {
       if (qParam && qParam.page) {
         this.currentPage = qParam.page;
       } else {
@@ -79,23 +78,27 @@ export class AllInvoiceComponent implements OnInit {
     });
     this.getAllInvoices();
   }
-  getAllInvoices(){
+  getAllInvoices() {
     const pagination: Pagination = {
       pageSize: this.invoicePerPage.toString(),
-      currentPage: this.currentPage.toString()
+      currentPage: this.currentPage.toString(),
     };
-    this.invoiceService.getAllInvoices(pagination, this.sortQuery, this.filter)
-    .subscribe(res => {
-      this.invoices = res.data;
-    }, err=>{
-      console.log(err);
-    })
+    this.invoiceService
+      .getAllInvoices(pagination, this.sortQuery, this.filter)
+      .subscribe(
+        (res) => {
+          this.invoices = res.data;
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
   }
 
   /**
    * SORTING
    */
-   sortData(query: any, type: number) {
+  sortData(query: any, type: number) {
     this.sortQuery = query;
     this.activeSort = type;
     this.getAllInvoices();
@@ -105,37 +108,39 @@ export class AllInvoiceComponent implements OnInit {
    * FILTER DATA
    */
 
-   filterByOrderStatus(data){
-    this.filter = {deliveryStatus: data};
+  filterByOrderStatus(data) {
+    this.filter = { deliveryStatus: data };
     this.getAllInvoices();
   }
 
-  filterByPaymentStatus(data: number){
-    this.filter = {paymentStatus: data};
+  filterByPaymentStatus(data: number) {
+    this.filter = { paymentStatus: data };
     this.getAllInvoices();
   }
 
   /**
    * PAGINATION CHANGE
    */
-   public onPageChanged(event: any) {
-    this.router.navigate([], {queryParams: {page: event}});
+  public onPageChanged(event: any) {
+    this.router.navigate([], { queryParams: { page: event } });
   }
 
   endChangeRegDateRange(event: MatDatepickerInputEvent<any>) {
     if (event.value) {
-      const startDate = this.utilsService.getDateString(this.dataFormDateRange.value.start);
-      const endDate = this.utilsService.getDateString(this.dataFormDateRange.value.end);
-      const qData = {createdAt: {$gte: startDate, $lte: endDate}};
+      const startDate = this.utilsService.getDateString(
+        this.dataFormDateRange.value.start
+      );
+      const endDate = this.utilsService.getDateString(
+        this.dataFormDateRange.value.end
+      );
+      const qData = { createdAt: { $gte: startDate, $lte: endDate } };
       this.filter = qData;
 
       if (this.currentPage > 1) {
-        this.router.navigate([], {queryParams: {page: 1}});
+        this.router.navigate([], { queryParams: { page: 1 } });
       } else {
         this.getAllInvoices();
       }
     }
   }
-
-
 }
