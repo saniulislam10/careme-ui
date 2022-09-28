3abceef35cde9f377a3d9ddc1d0687a7753e48fc
mamun07 <mamunben7@gmail.com>
2022-09-28 11:43:36 +0600

    Refund details

--- src/app/admin/pages/order/refund/refund-routing.module.ts
+++ src/app/admin/pages/order/refund/refund-routing.module.ts
@@ -1,11 +1,16 @@
 import { RefundComponent } from './refund.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
 
 const routes: Routes = [
   {
     path: '',
     component: RefundComponent,
+  },
+  {
+    path: 'details',
+    component: DetailsComponent,
   },
 ];
 
--- src/app/admin/pages/order/refund/refund.module.ts
+++ src/app/admin/pages/order/refund/refund.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { RefundRoutingModule } from './refund-routing.module';
 import { RefundComponent } from './refund.component';
 import { AntModule } from 'src/app/material/ant.module';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
-  declarations: [RefundComponent],
+  declarations: [RefundComponent, DetailsComponent],
   imports: [CommonModule, RefundRoutingModule, AntModule],
 })
 export class RefundModule {}
