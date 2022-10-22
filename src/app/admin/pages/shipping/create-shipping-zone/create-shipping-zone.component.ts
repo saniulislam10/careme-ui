5ee9330a25637aa1b66e2133d35ebda668bd16d5
mamun07 <mamunben7@gmail.com>
2022-10-22 11:10:41 +0600

    shipping select

--- src/app/admin/pages/shipping/create-shipping-zone/create-shipping-zone.component.ts
+++ src/app/admin/pages/shipping/create-shipping-zone/create-shipping-zone.component.ts
@@ -3,14 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create-shipping-zone',
   templateUrl: './create-shipping-zone.component.html',
-  styleUrls: ['./create-shipping-zone.component.scss']
+  styleUrls: ['./create-shipping-zone.component.scss'],
 })
 export class CreateShippingZoneComponent implements OnInit {
+  // For Details
+  zoneVisible = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  // For Details
+  showZoneModal(): void {
+    this.zoneVisible = true;
   }
-
-
+  zoneOk(): void {
+    console.log('Button ok clicked!');
+    this.zoneVisible = false;
+  }
+  zoneCancel(): void {
+    console.log('Button cancel clicked!');
+    this.zoneVisible = false;
+  }
 }
