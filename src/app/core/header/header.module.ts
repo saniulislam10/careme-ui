5b0959949fed39ef8f91026b7c84cb0362f7172b
mamun07 <mamunben7@gmail.com>
2022-10-30 12:00:51 +0600

    header menu

--- src/app/core/header/header.module.ts
+++ src/app/core/header/header.module.ts
@@ -5,9 +5,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 import { MenuHoverContentComponent } from './menu-hover-content/menu-hover-content.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzUploadModule } from 'ng-zorro-antd/upload';
+import { AntModule } from 'src/app/material/ant.module';
 
 @NgModule({
   declarations: [HeaderComponent, MenuHoverContentComponent],
@@ -17,9 +15,7 @@ import { NzUploadModule } from 'ng-zorro-antd/upload';
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
-    NzInputModule,
-    NzIconModule,
-    NzUploadModule,
+    AntModule,
   ],
   exports: [HeaderComponent],
 })
